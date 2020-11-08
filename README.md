73e48bad4bc5e807ad7c6abe0bdb7f2f97fd781f
Sebastian Aldi <sebastian.aldi17@gmail.com>
2020-11-08 22:19:40 +0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AutoMemer
-Sends memes from r/dankmemes to discord using Reddit API and discord.js
+Sends memes from r/dankmemes to discord using Reddit API and discord.js.
+Feel free to host this bot using Heroku, make sure to remember to change the BOT_TOKEN and CHANNEL_ID to your bot's secret and the channel for the memes.
---
+++ bot.js
@@ -0,0 +1,49 @@
+// https://www.reddit.com/r/dankmemes/top/.json?t=day
+
+const Axios = require('axios');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+let memes = []
+let memeIndex = 0
+let memeChannel = ""
+
+function refreshMemes() {
+    memes = []
+    memeIndex = 0
+    Axios.get('https://www.reddit.com/r/dankmemes/top/.json?t=day?limit=25')
+    .then((response) => {
+        let results = response.data.data
+        let children = results.children
+        children.forEach(element => {
+            memes.push(element.data.url)
+        });
+        sendMeme()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+function sendMeme() {
+    memeChannel.send(memes[memeIndex])
+    memeIndex += 1
+    if(memeIndex >= memes.length) {
+        refreshMemes()
+    }
+}
+
+client.on('ready', () => {
+    console.log('AutoMemer online.');
+    memeChannel = client.channels.cache.get(process.env.CHANNEL_ID)
+    refreshMemes()
+    setInterval(sendMeme, 7200000)
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+});
+
+client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
