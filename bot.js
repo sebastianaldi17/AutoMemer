efd034ddc39ff1f14d1553892e93ea468ce9c250
Sebastian Aldi <sebastian.aldi17@gmail.com>
2020-12-16 22:16:04 +0700

    Added tokopedia search function
    Added tokopedia search via web scrapping using Axios and Cheerio (new dependency for parsing HTML).

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 // https://www.reddit.com/r/dankmemes/top/.json?t=day
 
 const Axios = require('axios');
+const cheerio = require('cheerio')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -8,6 +9,31 @@ let memes = []
 let memeIndex = 0
 let memeInterval = 1000 * parseInt(process.env.SECONDS)
 let memeChannel = ""
+
+function tokopediaSearch(keywords, channel) {
+    // Last working: 16 December 2020
+    let combined = keywords.concat('+')
+    let url = `https://www.tokopedia.com/search?st=product&q=${combined}`
+    Axios.get(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const products = $('.css-18c4yhp')
+        const prices = $('.css-rhd610')
+        let string = ''
+        for(let i = 0; i < Math.min(products.length, 5); i++) {
+            console.log(products[i].children[0].data)
+            console.log(prices[i].children[0].data)
+            string += `${products[i].children[0].data} - ${prices[i].children[0].data} \n`
+        }
+        if(string.length > 0) channel.send(string)
+        else channel.send('No match found.')
+    })
+    .catch(error => {
+        channel.send('An error has occured while scrapping the data from Tokopedia.')
+        console.log(error)
+    })
+}
 
 function refreshMemes() {
     memes = []
@@ -51,14 +77,20 @@ function sendMeme() {
 
 client.on('ready', () => {
     console.log('AutoMemer online.');
+    client.user.setActivity('!tokped <keywords>')
     memeChannel = client.channels.cache.get(process.env.CHANNEL_ID)
     refreshMemes()
     setInterval(sendMeme, memeInterval)
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+    let contents = message.content.split(' ')
+    if(contents[0] === '!tokped') {
+        if(contents.length > 1) {
+            tokopediaSearch(contents.slice(1), message.channel)
+        } else {
+            message.channel.send('Please insert additional keywords.')
+        }
     }
 });
 
